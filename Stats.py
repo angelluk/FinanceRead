228ed23051babe8d79351b27a59d1504f011822f
Lukasus <lukasus@iMac-Lukasus.local>
2016-07-02 15:13:14 -0700

    add Bollinger bands

--- Stats.py
+++ Stats.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 '''
     read some stocks
 '''
+
+
 def get_data(symbols, dates):
 
     # create empty data frame
@@ -24,16 +26,15 @@ def get_data(symbols, dates):
 
         df1 = df1.dropna()
 
-        df1 = df1.iloc[::-1]  # reverse data frame in date
-
-        #df1 = df1 / df1.ix[0]  # normalize
-
+        df1 = df1.iloc[::-1]  # reverse data frame in data
 
     return df1
+
 
 '''
     plot data
 '''
+
 
 def plot_data(df):
 
@@ -44,15 +45,45 @@ def plot_data(df):
     plt.show()
 
 
+'''
+    return rolling mean
+'''
+
+
+def get_rolling_mean(values, window):
+    return pd.Series.rolling(values, window=window, center=True).mean()
+
+
+'''
+    return rolling sd
+'''
+
+
+def get_rolling_sd(values, window):
+    return pd.Series.rolling(values, window=window, center=True).std()
+
+
+'''
+    return upper and lower bands
+'''
+
+
+def get_bollinger_bands(rm, sd):
+    return rm + 2 * sd, rm - 2 * sd
+
+
+'''
+    TEST ROLLING MEAN
+'''
+
 
 def test_run():
 
-    dates = pd.date_range('2000-01-01', '2016-01-01')
+    dates = pd.date_range('2000-01-01', '2016-05-20')
 
     symbols = ['SPY', 'IBM', 'GOOG', 'GLD']
 
     df = get_data(symbols, dates)
-
 
     print "\n  Mean: \n", df.mean()
 
@@ -60,17 +91,17 @@ def test_run():
 
     print "\n  Standard deviation: \n", df.std()
 
-    # plot all stocks
-    #plot_data(df)
-
+    # get stack data
     df1 = get_data(['SPY'], dates)
 
+    # plot of SPY
     ax = df1['SPY'].plot(title='SPY rolling mean', label='SPY')
 
-    #rm_SPY = pd.rolling_mean(df1['SPY'], window=252)
-    rm_SPY =  pd.Series.rolling(df1['SPY'], window=252, center=False).mean()
+    # get rolling mean
+    rm_spy = pd.Series.rolling(df1['SPY'], window=252, center=True).mean()
 
-    rm_SPY.plot(label='Rolling mean', ax=ax)
+    # plot rolling mean with graph of SPY (ax=ax)
+    rm_spy.plot(label='Rolling mean', ax=ax)
 
     ax.set_xlabel('Date')
     ax.set_ylabel('Price')
@@ -80,6 +111,39 @@ def test_run():
 
 
 
+'''
+    TEST BB
+'''
+
+
+def test_Bollinder_Band():
+
+    dates = pd.date_range('2000-01-01', '2016-05-20')
+
+    # get stack data
+    df = get_data(['SPY'], dates)
+
+    # 1 compute rolling mean
+    rm_SPY = get_rolling_mean(df['SPY'], window=200)
+
+    # 2 compute rolling standart deviation
+    rm_sd = get_rolling_sd(df['SPY'], window=200)
+
+    # 3 compute upper and lower bands
+    upper_band, lower_band = get_bollinger_bands(rm_SPY, rm_sd)
+
+    # Plot SPY, mean and Bollinger Bands
+    ax = df['SPY'].plot(title='Bollinger Band', label='SPY')
+    rm_SPY.plot(label='Rolling mean', ax=ax)
+    upper_band.plot(label='upper band', ax=ax)
+    lower_band.plot(label='lower band', ax=ax)
+
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Price')
+    ax.legend(loc='upper left')
+    plt.show()
+
 if __name__ == "__main__":
 
-    test_run()
+    #test_run()
+    test_Bollinder_Band()
